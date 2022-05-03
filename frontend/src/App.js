acb7382df38b9b3919410e32be18981d9feb8ab3
Franco Vazquez <82483907+Fraanv1@users.noreply.github.com>
2022-05-03 00:13:08 -0300

    Final del proyecto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,26 @@
 import logo from './logo.svg'
 import './App.css'
 
+//importamos componentes
+import CompShowBlogs from './blog/ShowBlogs'
+import CompCreateBlog from './blog/CreateBlog'
+import CompEditBlog from './blog/EditBlog'
+
+//importacion de router
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-				<button className='btn btn-primary btn-lg'>
-					<i className='fab fa-react'></i>
-				</button>
 			</header>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<CompShowBlogs />} />
+					<Route path='/create' element={<CompCreateBlog />} />
+					<Route path='/edit/:id' element={<CompEditBlog />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	)
 }
